c74450e1103b0ace676d7142719ddcce34ef8d95
Philippe Lécaillon <lecaillon@hotmail.com>
2019-06-08 20:09:16 +0200

    Added new tests

--- test/Cassette.Tests/IntegrationTests.cs
+++ test/Cassette.Tests/IntegrationTests.cs
@@ -10,10 +10,11 @@ using Xunit;
 
 namespace Cassette.Tests
 {
-    public class ReplayingHandlerTest : IClassFixture<WebApplicationFactory<Startup>>, IClassFixture<CustomWebApplicationFactory<Startup>>
+    public class IntegrationTests : IClassFixture<WebApplicationFactory<Startup>>, IClassFixture<CustomWebApplicationFactory<Startup>>
     {
         private const string KeyGetRegion11Deps = "Cassette:Tests:GET:https//geo.api.gouv.fr/regions/11/departements:02j5xk5DetP1iTdH+UIGnE25214=";
         private const string KeyGetRegionWithLastSegmentExcluded = "Cassette:Tests:GET:https//geo.api.gouv.fr/regions/:qxrE8t5NkfBXyxPRiOe8hfY2dhE=";
+        private const string KeyGetAllRegions = "Cassette:Tests:GET:https//geo.api.gouv.fr/regions:Oc06qZzKHX4uELWVZo46hBoCAoc=";
 
         // 1: NoCache
         private readonly IGeoApi _geoApi1;
@@ -24,7 +25,7 @@ namespace Cassette.Tests
         private readonly IDistributedCache _cache2;
         private readonly CassetteOptions _options2;
 
-        public ReplayingHandlerTest(WebApplicationFactory<Startup> factory1, CustomWebApplicationFactory<Startup> factory2)
+        public IntegrationTests(WebApplicationFactory<Startup> factory1, CustomWebApplicationFactory<Startup> factory2)
         {
             _geoApi1 = RestService.For<IGeoApi>(factory1.CreateClient());
             _cache1 = factory1.Server.Host.Services.GetRequiredService<IDistributedCache>();
@@ -55,13 +56,6 @@ namespace Cassette.Tests
         }
 
         [Fact]
-        public async Task Should_Not_Record_Anything_When_Cassette_Is_Not_Activated()
-        {
-            await _geoApi1.GetDepartements("11");
-            (await _cache1.GetAsync(KeyGetRegion11Deps)).Should().BeNull("The cache should be empty, because Cassette is not activated.");
-        }
-
-        [Fact]
         public async Task Should_Exclude_Last_Segment_Of_The_Uri_From_The_Key_Cache()
         {
             var region93 = await _geoApi2.GetRegion("93");
@@ -71,7 +65,21 @@ namespace Cassette.Tests
             var region44 = await _geoApi2.GetRegion("44");
             byte[] cassetteB = await _cache2.GetAsync(KeyGetRegionWithLastSegmentExcluded);
             cassetteB.Should().NotBeNull("The cassette should already be in the cache due to the ExcludeLastUriSegment HTTP header.");
-            region44.Should().BeEquivalentTo(region93, "What ever the given region code the response should be region93 because of the ExcludeLastUriSegment configuration.");
+            region44.Should().BeEquivalentTo(region93, "What ever the given region code, the response should be region93 because [ExcludeLastUriSegment] is used.");
+        }
+
+        [Fact]
+        public async Task Should_Not_Record_Anything_When_Cassette_Is_Not_Activated()
+        {
+            await _geoApi1.GetDepartements("11");
+            (await _cache1.GetAsync(KeyGetRegion11Deps)).Should().BeNull("The cache should be empty, because Cassette is not activated.");
+        }
+
+        [Fact]
+        public async Task Should_Not_Record_Anything_When_NoRecord_Is_Used()
+        {
+            await _geoApi2.GetRegions();
+            (await _cache2.GetAsync(KeyGetAllRegions)).Should().BeNull("The cache should be empty, because [NoRecord] is used.");
         }
 
         private Task WaitHalftTheDurationOfTheExpirationTime() 
